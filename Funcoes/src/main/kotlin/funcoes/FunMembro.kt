fd031f8d83e2bc9782f6bdbddf7ba400de147be2
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2024-09-04 19:12:12 -0300

    Funções membro

--- Funcoes/src/main/kotlin/funcoes/FunMembro.kt
+++ Funcoes/src/main/kotlin/funcoes/FunMembro.kt
@@ -1,2 +1,26 @@
 package funcoes
 
+object Rectangle{
+    fun printArea(with: Int, height: Int): Unit{
+        val area = calucuteArea(with, height)
+        println("Essa area é :$area")
+    }
+
+    fun calucuteArea(with: Int, height: Int): Int{
+        return with * height
+    }
+}
+
+fun main(){
+
+    val string = "Hello"
+    val length = string.take(3)
+    println(length)
+    println()
+    val  result = Rectangle
+
+    println(result.printArea(88, 68))
+    println()
+    println(result.printArea(9, 6))
+
+}
